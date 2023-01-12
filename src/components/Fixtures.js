9839b036910f186f33a6e90664041aa1dd6a328f
Denis Moini <denismoini09@gmail.com>
2023-01-13 00:13:55 +0300

    pitch-layout

--- src/components/Fixtures.js
+++ src/components/Fixtures.js
@@ -1,8 +1,22 @@
-import React from 'react'
 
 function Fixtures() {
   return (
-    <div>Fixtures</div>
+    <div className="fixtures-col">
+			<h4 className="large fixture-heading">Fixture Ticker</h4>
+			<div className="fixture-ticker">
+            <div className="next-fixtures">
+					<label className="small">Next:</label>
+					<select className="custom-select custom-select-next" id="nxt_fixtures">
+					</select>
+					<span className="small">Fixtures</span>
+				</div>
+				<table className="ticker-table">
+					<tr>
+						<td><i>Loading...</i></td>
+					</tr>
+				</table>
+			</div>
+		</div>
   )
 }
 
