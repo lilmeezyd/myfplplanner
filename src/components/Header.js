e41a358b609e7ccb5d147c53403b9f7ad9dc41aa
Denis Moini <denismoini09@gmail.com>
2023-04-08 13:42:47 +0300

    added game uodating

--- src/components/Header.js
+++ src/components/Header.js
@@ -65,7 +65,7 @@ function Header(props) {
 		<form onSubmit={getManagerId} name="team">
 			<label htmlFor="team">Team ID</label>
 			<input onChange={(e) => setManagerId(+e.target.value)} type="number" id="teamid" required/>
-			<button type="submit" className="btn btn-block btn-fpl">Load</button>
+			<button type="submit" className="btn btn-fpl btn-block">Load</button>
 		</form>
 		<button onClick={handleCloseModal} className="btn-info btn-close btn-danger" id="close_modal"><svg style={{color: 'white'}} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16"> <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" fill="white"></path> </svg></button>
 	</div>}
