3febf2cfe34a17e16b2f871b92a0273662b6e10d
Denis Moini <denismoini09@gmail.com>
2023-04-15 02:00:56 +0300

    added react-app-polyfill

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
