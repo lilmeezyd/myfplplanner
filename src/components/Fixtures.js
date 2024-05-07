59497f4db5c756f2bcd938292b074702545fdb31
Denis Moini <denismoini09@gmail.com>
2024-05-08 02:36:15 +0300

    added-gh-pages

--- src/components/Fixtures.js
+++ src/components/Fixtures.js
@@ -1,7 +1,9 @@
 import { useContext, useState, Suspense } from "react";
 import { BootstrapstaticContext } from "../BootstrapstaticContext";
 import { numberOfFixtures, loadStartGw } from "../services/fixtureService";
+import { loadOpponents } from "../services/fixtureService";
 import Loader from "./Loader";
+import TeamRowOpponents from "./TeamRowOpponents";
 import TeamRow from "./TeamRow";
 
 function Fixtures() {
@@ -78,11 +80,59 @@ function Fixtures() {
               {/* Problem here */}
                   {/*<TeamRow gws={gws} start={start} team={team} key={team.id} />*/}
               <tbody className="small triple">
-                {teams.map((team) => (
+                {teams.map((team) => {
+                  const opponents = loadOpponents(
+                    fixtures,
+                    events,
+                    team.id,
+                    gws,
+                    start
+                  ).newTeamAandH;
+                  return (
                   <tr>
-                    <td>{team.name}</td>
-                  </tr>
-                ))}
+                  <td>
+                    <div className="team-row">
+                      <span className="ticker-image">
+                        <img src={require(`../static/t${team.code}.png`)} alt={team.name} />
+                      </span>
+              <span className="ticker-team">{team.name}</span>
+                    </div>
+                  </td>
+                  {opponents.map((cell) => (
+                    <td key={cell.event}>
+                      <div className="oppfix">
+                        {cell.arr.map((x, idx) => (
+                          <TeamRowOpponents
+                            key={x.opponent}
+                            color={
+                              x.difficulty === 4 || x.difficulty === 5
+                                ? "rgb(255,255,255)"
+                                : "rgb(0,0,0)"
+                            }
+                            backgroundColor={
+                              x.difficulty === 2
+                                ? "rgb(1, 252, 122)"
+                                : x.difficulty === 3
+                                ? "rgb(231, 231, 231)"
+                                : x.difficulty === 4
+                                ? "rgb(255, 23, 81)"
+                                : x.difficulty === 5
+                                ? "rgb(128, 7, 45)"
+                                : "rgb(0,0,0)"
+                            }
+                            name={
+                              x.opponent > 0
+                                ? teams.filter((y) => y.id === x.opponent)[0].short_name
+                                : ""
+                            }
+                            venue={x.venue}
+                          />
+                        ))}
+                      </div>
+                    </td>
+                  ))}
+                </tr>)
+})}
               </tbody>
               {/* Problem here */}
             </table>
