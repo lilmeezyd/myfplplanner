a1b20842fbafed4bc457869ea7e243d3115f97d1
Denis Moini <denismoini09@gmail.com>
2024-05-08 03:13:21 +0300

    added-gh-pages

--- src/components/Fixtures.js
+++ src/components/Fixtures.js
@@ -107,31 +107,22 @@ function Fixtures() {
                     <td key={cell.event}>
                       <div className="oppfix">
                         {cell.arr.map((x, idx) => (
-                          <TeamRowOpponents
-                            key={x.opponent}
-                            color={
-                              x.difficulty === 4 || x.difficulty === 5
-                                ? "rgb(255,255,255)"
-                                : "rgb(0,0,0)"
-                            }
-                            backgroundColor={
-                              x.difficulty === 2
-                                ? "rgb(1, 252, 122)"
-                                : x.difficulty === 3
-                                ? "rgb(231, 231, 231)"
-                                : x.difficulty === 4
-                                ? "rgb(255, 23, 81)"
-                                : x.difficulty === 5
-                                ? "rgb(128, 7, 45)"
-                                : "rgb(0,0,0)"
-                            }
-                            name={
-                              x.opponent > 0
-                                ? teams.filter((y) => y.id === x.opponent)[0].short_name
-                                : ""
-                            }
-                            venue={x.venue}
-                          />
+                          <span className='opponent' 
+                          style={{color: x.difficulty === 4 || x.difficulty === 5
+                            ? "rgb(255,255,255)"
+                            : "rgb(0,0,0)", backgroundColor: 
+                            x.difficulty === 2
+                            ? "rgb(1, 252, 122)"
+                            : x.difficulty === 3
+                            ? "rgb(231, 231, 231)"
+                            : x.difficulty === 4
+                            ? "rgb(255, 23, 81)"
+                            : x.difficulty === 5
+                            ? "rgb(128, 7, 45)"
+                            : "rgb(0,0,0)"}}>{x.opponent > 0
+                              ? teams.filter((y) => y.id === x.opponent)[0].short_name
+                              : ""}{x.venue}</span>
+                          
                         ))}
                       </div>
                     </td>
--- src/components/TeamRowOpponents.js
+++ src/components/TeamRowOpponents.js
@@ -3,7 +3,34 @@ import React from 'react'
 function TeamRowOpponents(props) {
     const { color, backgroundColor, name, venue} = props
   return (
+    <>
+    {/*<TeamRowOpponents
+                            key={x.opponent}
+                            color={
+                              x.difficulty === 4 || x.difficulty === 5
+                                ? "rgb(255,255,255)"
+                                : "rgb(0,0,0)"
+                            }
+                            backgroundColor={
+                              x.difficulty === 2
+                                ? "rgb(1, 252, 122)"
+                                : x.difficulty === 3
+                                ? "rgb(231, 231, 231)"
+                                : x.difficulty === 4
+                                ? "rgb(255, 23, 81)"
+                                : x.difficulty === 5
+                                ? "rgb(128, 7, 45)"
+                                : "rgb(0,0,0)"
+                            }
+                            name={
+                              x.opponent > 0
+                                ? teams.filter((y) => y.id === x.opponent)[0].short_name
+                                : ""
+                            }
+                            venue={x.venue}
+                          />*/}
     <span className='opponent' style={{color: color, backgroundColor: backgroundColor}}>{name}{venue}</span>
+    </>
   )
 }
 
