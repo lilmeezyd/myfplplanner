d7893ba0337f15e28d920095baf03ffb18b3d37b
Denis Moini <denismoini09@gmail.com>
2024-05-08 03:39:45 +0300

    added-gh-pages

--- src/components/Fixtures.js
+++ src/components/Fixtures.js
@@ -81,7 +81,7 @@ function Fixtures() {
                   {/*<TeamRow gws={gws} start={start} team={team} key={team.id} />*/}
               <tbody className="small triple">
                 {teams.map(team => 
-                <tr>
+                <tr key={team.id}>
                   <td>
                   <div className="team-row">
                     <span className="ticker-image">
@@ -89,14 +89,8 @@ function Fixtures() {
                     </span>
             <span className="ticker-team">{team.name}</span>
                   </div>
-                </td>{/*
-                const opponents = loadOpponents(
-                    fixtures,
-                    events,
-                    team.id,
-                    gws,
-                    start
-                  ).newTeamAandH;*/}
+                </td>
+                <>
                   {loadOpponents(
                     fixtures,
                     events,
@@ -126,62 +120,10 @@ function Fixtures() {
                         ))}
                       </div>
                     </td>
-                  ))}
+                  ))}</>
                 </tr>
                 )}
-                {/*teams.map((team) => {
-                  const opponents = loadOpponents(
-                    fixtures,
-                    events,
-                    team.id,
-                    gws,
-                    start
-                  ).newTeamAandH;
-                  (
-                  <tr>
-                  <td>
-                    <div className="team-row">
-                      <span className="ticker-image">
-                        <img src={require(`../static/t${team.code}.png`)} alt={team.name} />
-                      </span>
-              <span className="ticker-team">{team.name}</span>
-                    </div>
-                  </td>
-                  {opponents.map((cell) => (
-                    <td key={cell.event}>
-                      <div className="oppfix">
-                        {cell.arr.map((x, idx) => (
-                          <TeamRowOpponents
-                            key={x.opponent}
-                            color={
-                              x.difficulty === 4 || x.difficulty === 5
-                                ? "rgb(255,255,255)"
-                                : "rgb(0,0,0)"
-                            }
-                            backgroundColor={
-                              x.difficulty === 2
-                                ? "rgb(1, 252, 122)"
-                                : x.difficulty === 3
-                                ? "rgb(231, 231, 231)"
-                                : x.difficulty === 4
-                                ? "rgb(255, 23, 81)"
-                                : x.difficulty === 5
-                                ? "rgb(128, 7, 45)"
-                                : "rgb(0,0,0)"
-                            }
-                            name={
-                              x.opponent > 0
-                                ? teams.filter((y) => y.id === x.opponent)[0].short_name
-                                : ""
-                            }
-                            venue={x.venue}
-                          />
-                        ))}
-                      </div>
-                    </td>
-                  ))}
-                </tr>)
-})*/}
+                {/**/}
               </tbody>
               {/* Problem here */}
             </table>
